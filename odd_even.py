b92166438eebf7c90e36b915c5aa454357e2f0cb
ssanthu49 <ssanthu49@gmail.com>
2020-04-28 18:18:42 +0530

    Modified in Odd & Even

--- odd_even.py
+++ odd_even.py
@@ -1,7 +1,7 @@
 '''def is_even(n):
     return n%2!=0'''
 from functools import reduce 
-nums=[2,6,3,5,1,8,9,10]
+nums=[2,6,3,5,1,8,9,10,14,17]
 print(nums)
 even=list(filter(lambda n : n%2!=0, nums))
 db= list(map(lambda n : n+2, nums))
